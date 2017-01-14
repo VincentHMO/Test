3b608439b6b002dd57ae1a769ebbd7bd0d5cd5b9
VincentHMO <v2.fluhmann@bluewin.ch>
2017-01-14 21:38:11 +0100

    Essai avec une branche

--- HmoTimeTracker/HmoTimeTracker/Form1.Designer.cs
+++ HmoTimeTracker/HmoTimeTracker/Form1.Designer.cs
@@ -33,11 +33,11 @@
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Location = new System.Drawing.Point(197, 226);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Quitter";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
